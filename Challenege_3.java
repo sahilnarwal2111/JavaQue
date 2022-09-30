2ea28286bba464b740cae5510a682403c0e94361
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-09-30 23:14:35 +0530

    updated

--- Challenege_3.java
+++ Challenege_3.java
@@ -32,6 +32,7 @@ class Result {
 
     public static void solve(double meal_cost, int tip_percent, int tax_percent) {
         // Write your code here
+            
         total_cost =(int) meal_cost + meal_cost*tip_percent/100 + meal_cost*tax_percent/100;
         System.out.println((int)total_cost);
 
