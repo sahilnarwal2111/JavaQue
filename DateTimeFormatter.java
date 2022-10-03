f8506219de247fcae29c726725e748b8fb730e88
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-03 22:51:37 +0530

    Update DateTimeFormatter.java

--- DateTimeFormatter.java
+++ DateTimeFormatter.java
@@ -1,14 +1,10 @@
 import java.time.LocalDateTime;
-
 public class DateTimeFormatter {
     public static void main(String[] args) {
         LocalDateTime dt  = LocalDateTime.now();
-        System.out.println(dt);
-
-        
+        System.out.println(dt);       
 //        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 //        String myDate = dt.format(df);
 //        System.out.println(myDate);
     }
-
 }
